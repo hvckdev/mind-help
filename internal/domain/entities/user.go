6679024b512144987438840d7f7348313b0c5d47
hvck <i@hvck.dev>
2024-12-05 22:25:01 +0300

    feat: add question and diary models

--- internal/domain/entities/user.go
+++ internal/domain/entities/user.go
@@ -10,3 +10,24 @@ type User struct {
 	RegisteredAt time.Time `json:"registeredAt"`
 	UpdatedAt    time.Time `json:"updatedAt"`
 }
+
+type Question struct {
+	ID        int64     `json:"id,omitempty"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Owner     User      `json:"owner"`
+}
+
+type Diary struct {
+	ID        int64     `json:"ID"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Owner     User      `json:"owner"`
+}
+
+type DiaryQuestion struct {
+	Question Question `json:"question"`
+	Diary    Diary    `json:"diary"`
+}
